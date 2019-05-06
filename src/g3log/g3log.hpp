fff5b7613088c8763ab94c9cabc2d74ef2ba61b5
shiyue <shiyue@fabu.ai>
2019-05-06 14:58:27 +0800

    fix DCHECK

--- src/g3log/g3log.hpp
+++ src/g3log/g3log.hpp
@@ -222,6 +222,41 @@ bool shutDownLoggingForActiveOnly(LogWorker *active);
 #define DCHECK_GE(val1, val2) CHECK_GE(val1, val2)
 #define DCHECK_GT(val1, val2) CHECK_GT(val1, val2)
 #define DCHECK_NOTNULL(val) CHECK_NOTNULL(val)
+#else
+// should use while instead of if
+#define DLOG(level)                                                            \
+  if constexpr (false)                                                         \
+  GLOG_LOG(level)
+#define DVLOG(verboselevel)                                                    \
+  if constexpr (false)                                                         \
+  GLOG_VLOG(verboselevel)
+#define DLOG_IF(level, condition)                                              \
+  if constexpr (false)                                                         \
+  GLOG_LOG_IF(level, condition)
+#define DCHECK(condition)                                                      \
+  if constexpr (false)                                                         \
+  CHECK(condition)
+#define DCHECK_EQ(val1, val2)                                                  \
+  if constexpr (false)                                                         \
+  CHECK_EQ(val1, val2)
+#define DCHECK_NE(val1, val2)                                                  \
+  if constexpr (false)                                                         \
+  CHECK_NE(val1, val2)
+#define DCHECK_LE(val1, val2)                                                  \
+  if constexpr (false)                                                         \
+  CHECK_LE(val1, val2)
+#define DCHECK_LT(val1, val2)                                                  \
+  if constexpr (false)                                                         \
+  CHECK_LT(val1, val2)
+#define DCHECK_GE(val1, val2)                                                  \
+  if constexpr (false)                                                         \
+  CHECK_GE(val1, val2)
+#define DCHECK_GT(val1, val2)                                                  \
+  if constexpr (false)                                                         \
+  CHECK_GT(val1, val2)
+#define DCHECK_NOTNULL(val)                                                    \
+  if constexpr (false)                                                         \
+  CHECK_NOTNULL(val)
 #endif
 
 /** For details please see this
--- src/g3log/sinks/custom_sink.hpp
+++ src/g3log/sinks/custom_sink.hpp
@@ -1,3 +1,5 @@
+#ifndef G3LOG_SINKS_CUSTOMSINK_H_
+#define G3LOG_SINKS_CUSTOMSINK_H_
 #include "g3log/g3log.hpp"
 #include "g3log/loglevels.hpp"
 #include "g3log/logworker.hpp"
@@ -8,3 +10,5 @@ const LEVELS ERROR{WARNING.value + 10, "ERROR"};
 namespace g3 {
 void InitG3Logging(const char *prefix);
 } // namespace g3
+
+#endif
